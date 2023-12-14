59e82640a52f753eba1b1d2778eabc58ebec81fa
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-12-14 15:21:30 +0100

    ProductCard compoent basics created, format.ts is created to format the price

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,25 @@
+import { Product } from "@prisma/client"
+import Link from "next/link"
+
+interface ProductCardProps {
+    product: Product
+}
+
+const ProductCard = ({product}: ProductCardProps) => {
+    return (
+        <Link
+            href={"/products/"+ product.id}
+            className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
+        >
+            <div className="card-body">
+                <h2 className="card-title">
+                    {product.name}
+                </h2>
+                <p>{product.description}</p>
+                
+            </div>
+        </Link>
+    )
+}
+
+export default ProductCard
