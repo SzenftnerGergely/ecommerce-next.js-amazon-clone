2dbce2c16ea2dfcdb9bd9812c021015feaffb13d
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-12-15 18:36:06 +0100

    createCart and getCart util functions are created

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,23 +1,20 @@
-"use client"
+'use client'
 
-import { BsCart2 } from "react-icons/bs"
+import { BsCart2 } from 'react-icons/bs'
 
 interface AddToCartButtonProps {
-    productId: string,
+  productId: string
 }
 
 const AddToCartButton = ({ productId }: AddToCartButtonProps) => {
-    return (
-        <div className="flex item-center gap-2">
-            <button
-                className="btn btn-primary"
-                onClick={() => {}}
-            >
-                Add to Cart
-                <BsCart2 className="text-lg" />
-            </button>
-        </div>
-    )
+  return (
+    <div className="item-center flex gap-2">
+      <button className="btn btn-primary" onClick={() => {}}>
+        Add to Cart
+        <BsCart2 className="text-lg" />
+      </button>
+    </div>
+  )
 }
 
 export default AddToCartButton
